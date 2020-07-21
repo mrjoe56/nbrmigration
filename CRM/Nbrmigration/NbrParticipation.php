a6ad8b68a29cd88c6e46e8a85bdb8ea531e6224c
ErikHommel <erik.hommel@civicoop.org>
2020-07-21 09:28:55 +0200

    locally tested participation migration with fake data

--- CRM/Nbrmigration/NbrParticipation.php
+++ CRM/Nbrmigration/NbrParticipation.php
@@ -14,26 +14,28 @@ class CRM_Nbrmigration_NbrParticipation {
    */
   public function __construct() {
     $fileName = "participation_migration_" . date("YmdhIs");
-    $this->_logger = new CRM_Nihrbackbone_NihrLogger($fileName);
-    $this->_participationCaseTypeId = (int) CRM_Nihrbackbone_BackboneConfig::singleton()->getParticipationCaseTypeId();
-    $this->_acceptedStatus = Civi::service('nbrBackbone')->getAcceptedParticipationStatusValue();
-    $this->_excludedStatus = Civi::service('nbrBackbone')->getExcludedParticipationStatusValue();
-    $this->_invitationPendingStatus = Civi::service('nbrBackbone')->getInvitationPendingParticipationStatusValue();
-    $this->_invitedStatus = Civi::service('nbrBackbone')->getInvitedParticipationStatusValue();
-    $this->_noResponseStatus = Civi::service('nbrBackbone')->getNoResponseParticipationStatusValue();
-    $this->_notParticipatedStatus = Civi::service('nbrBackbone')->getNotParticipatedParticipationStatusValue();
-    $this->_participatedStatus = Civi::service('nbrBackbone')->getParticipatedParticipationStatusValue();
-    $this->_refusedStatus = Civi::service('nbrBackbone')->getRefusedParticipationStatusValue();
-    $this->_renegedStatus = Civi::service('nbrBackbone')->getRenegedParticipationStatusValue();
-    $this->_returnToSenderStatus = Civi::service('nbrBackbone')->getReturnToSenderParticipationStatusValue();
-    $this->_selectedStatus = Civi::service('nbrBackbone')->getSelectedParticipationStatusValue();
-    $this->_withdrawnStatus = Civi::service('nbrBackbone')->getWithdrawnParticipationStatusValue();
-    $this->_studyIdCustomField = "custom_" . CRM_Nihrbackbone_BackboneConfig::singleton()->getParticipationCustomField('nvpd_study_id', 'id');
-    $this->_studyParticipantIdCustomField = "custom_" . CRM_Nihrbackbone_BackboneConfig::singleton()->getParticipationCustomField('nvpd_study_participant_id', 'id');
-    $this->_studyParticipationStatusCustomField = "custom_" . CRM_Nihrbackbone_BackboneConfig::singleton()->getParticipationCustomField('nvpd_study_participation_status', 'id');
-    $this->_dateInvitedCustomField = "custom_" . CRM_Nihrbackbone_BackboneConfig::singleton()->getParticipationCustomField('nvpd_date_invited', 'id');
-    $this->_recallGroupCustomField = "custom_" . CRM_Nihrbackbone_BackboneConfig::singleton()->getParticipationCustomField('nvpd_recall_group', 'id');
+    $this->logger = new CRM_Nihrbackbone_NihrLogger($fileName);
+    $this->participationCaseTypeId = (int) CRM_Nihrbackbone_BackboneConfig::singleton()->getParticipationCaseTypeId();
+    $this->acceptedStatus = Civi::service('nbrBackbone')->getAcceptedParticipationStatusValue();
+    $this->excludedStatus = Civi::service('nbrBackbone')->getExcludedParticipationStatusValue();
+    $this->invitationPendingStatus = Civi::service('nbrBackbone')->getInvitationPendingParticipationStatusValue();
+    $this->invitedStatus = Civi::service('nbrBackbone')->getInvitedParticipationStatusValue();
+    $this->noResponseStatus = Civi::service('nbrBackbone')->getNoResponseParticipationStatusValue();
+    $this->notParticipatedStatus = Civi::service('nbrBackbone')->getNotParticipatedParticipationStatusValue();
+    $this->participatedStatus = Civi::service('nbrBackbone')->getParticipatedParticipationStatusValue();
+    $this->refusedStatus = Civi::service('nbrBackbone')->getRefusedParticipationStatusValue();
+    $this->renegedStatus = Civi::service('nbrBackbone')->getRenegedParticipationStatusValue();
+    $this->returnToSenderStatus = Civi::service('nbrBackbone')->getReturnToSenderParticipationStatusValue();
+    $this->selectedStatus = Civi::service('nbrBackbone')->getSelectedParticipationStatusValue();
+    $this->withdrawnStatus = Civi::service('nbrBackbone')->getWithdrawnParticipationStatusValue();
+    $this->studyIdCustomField = "custom_" . CRM_Nihrbackbone_BackboneConfig::singleton()->getParticipationCustomField('nvpd_study_id', 'id');
+    $this->studyParticipantIdCustomField = "custom_" . CRM_Nihrbackbone_BackboneConfig::singleton()->getParticipationCustomField('nvpd_study_participant_id', 'id');
+    $this->studyParticipationStatusCustomField = "custom_" . CRM_Nihrbackbone_BackboneConfig::singleton()->getParticipationCustomField('nvpd_study_participation_status', 'id');
+    $this->dateInvitedCustomField = "custom_" . CRM_Nihrbackbone_BackboneConfig::singleton()->getParticipationCustomField('nvpd_date_invited', 'id');
+    $this->recallGroupCustomField = "custom_" . CRM_Nihrbackbone_BackboneConfig::singleton()->getParticipationCustomField('nvpd_recall_group', 'id');
     $this->sentToResearcherActTypeId = CRM_Nihrbackbone_BackboneConfig::singleton()->getExportExternalActivityTypeId();
+    $this->changeStatusActTypeId = CRM_Nihrbackbone_BackboneConfig::singleton()->getChangedStudyStatusActivityTypeId();
+    $this->meetingActTypeId = Civi::service('nbrBackbone')->getMeetingActivityTypeId();
   }
 
   /**
@@ -48,36 +50,151 @@ class CRM_Nbrmigration_NbrParticipation {
       // first find contact id with sample_id, log if none found
       $contactId = CRM_Nbrmigration_NbrUtils::getContactIdWithSampleId($sourceData->sample_id);
       if (!$contactId) {
-        $this->_logger->logMessage('No contact found with sample_id: ' . $sourceData->sample_id, 'error');
+        $this->logger->logMessage('No contact found with sample_id: ' . $sourceData->sample_id, 'error');
         return FALSE;
       }
       // next find study, log if none found
       $studyId = CRM_Nbrmigration_NbrUtils::getStudyIdWithStudyNumber($sourceData->study_number);
       if (!$studyId) {
-        $this->_logger->logMessage('No study found with study_number: ' . $sourceData->study_number, 'error');
+        $this->logger->logMessage('No study found with study_number: ' . $sourceData->study_number, 'error');
         return FALSE;
       }
-      // create case with custom data
-      try {
-        $createdCase = civicrm_api3('Case', 'create', $this->prepareParticipationData($contactId, $studyId, $sourceData));
-        // add sent to researcher activity if required
-        if (!empty($sourceData->sent_to_researcher)) {
-          $this->createSentToResearcher($createdCase['id'], $sourceData->sent_to_researcher);
+      // create case with custom data (only if volunteer is not already on study)
+      if (!CRM_Nihrbackbone_NbrVolunteerCase::isAlreadyOnStudy($contactId, $studyId)) {
+        try {
+          $createdCase = civicrm_api3('Case', 'create', $this->prepareParticipationData($contactId, $studyId, $sourceData));
+          // add sent to researcher activity if required
+          if (!empty($sourceData->sent_to_researcher)) {
+            $this->createCaseActivity($this->prepareSentData($createdCase['id'], $contactId, $sourceData));
+          }
+          // add change status from invited to participated activity if required
+          if (!empty($sourceData->project_participation_date_answered)) {
+            $this->createCaseActivity($this->prepareAnsweredData($createdCase['id'], $contactId, $sourceData));
+          }
+          // add meeting activity with note if required
+          if (!empty($sourceData->project_participation_notes)) {
+            $this->createCaseActivity($this->prepareNoteData($createdCase['id'], $contactId, $sourceData));
+          }
+          try {
+            civicrm_api3("Contact", "addidentity", [
+              'contact_id' => $contactId,
+              'identifier' => $sourceData->anon_study_participation_id,
+              'identifier_type' => CRM_Nihrnumbergenerator_Config::singleton()->studyParticipantIdIdentifier,
+            ]);
+          }
+          catch (CiviCRM_API3_Exception $ex) {
+            $this->logger->logMessage("Could not add study participant id ") . $sourceData->anon_study_participation_id
+            . " as new contact identifier for contact ID " . $contactId . ", error from API Contact addidentity: " . $ex->getMessage();
+          }
         }
-        // todo add change status to accepted activity if required
-        // todo add note activity if required
-        // todo add study participant id as contact identifier
+        catch (CiviCRM_API3_Exception $ex) {
+          $this->logger->logMessage('Error when trying to create case in: ' . __METHOD__ . ', API error message: ' . $ex->getMessage(), 'error');
+          return FALSE;
+        }
       }
-      catch (CiviCRM_API3_Exception $ex) {
-        $this->_logger->logMessage('Error when trying to create case in: ' . __METHOD__ . ', API error message: ' . $ex->getMessage(), 'error');
+      else {
+        $this->logger->logMessage('Volunteer with id: ' . $contactId . ' is already on study with id: ' . $studyId . ', not imported.', 'error');
         return FALSE;
       }
     }
   }
-  public function createSentToResearcher($caseId, $dateSent) {
-    $dateSent = new DateTime($dateSent);
 
+  /**
+   * Method to prepare note
+   *
+   * @param $caseId
+   * @param $contactId
+   * @param $sourceData
+   * @return array
+   */
+  private function prepareNoteData($caseId, $contactId, $sourceData) {
+    return [
+      'activity_type_id' => $this->meetingActTypeId,
+      'case_id' => $caseId,
+      'target_contact_id' => $contactId,
+      'status_id' => "Completed",
+      'subject' => "Note from Starfish (imported during migration)",
+      'details' => CRM_Core_DAO::escapeString(strip_tags($sourceData->project_participation_notes)),
+    ];
+  }
 
+  /**
+   * Method to prepare data for sent to researcher
+   *
+   * @param $caseId
+   * @param $contactId
+   * @param $sourceData
+   * @return array
+   * @throws Exception
+   */
+  private function prepareSentData($caseId, $contactId, $sourceData) {
+    $sentDate = new DateTime($sourceData->sent_to_researcher);
+    return [
+      'activity_type_id' => $this->sentToResearcherActTypeId,
+      'activity_date_time' => $sentDate->format('Y-m-d'),
+      'case_id' => $caseId,
+      'target_contact_id' => $contactId,
+      'status_id' => "Completed",
+      'subject' => "Exported to External Researcher(s) (imported during migration)",
+    ];
+  }
+
+  /**
+   * Method to prepare data for changed status to accepted
+   *
+   * @param $caseId
+   * @param $contactId
+   * @param $sourceData
+   * @return array
+   * @throws Exception
+   */
+  private function prepareAnsweredData($caseId, $contactId, $sourceData) {
+    $answeredDate = new DateTime($sourceData->project_participation_date_answered);
+    return [
+      'activity_type_id' => $this->changeStatusActTypeId,
+      'activity_date_time' => $answeredDate->format('Y-m-d'),
+      'case_id' => $caseId,
+      'target_contact_id' => $contactId,
+      'status_id' => "Completed",
+      'subject' => "Changed from status Invited to status Accepted (imported during migration)",
+    ];
+
+  }
+
+  /**
+   * Method to add activity to the migrated case
+   *
+   * @param array
+   * @throws Exception
+   */
+  public function createCaseActivity($activityData) {
+    // only if we have a case id and activity type id
+    if (isset($activityData['case_id']) && !empty($activityData['case_id']) && isset($activityData['activity_type_id']) && !empty($activityData['activity_type_id'])) {
+      if (!isset($activityData['activity_date_time']) || empty($activityData['activity_date_time'])) {
+        $activityDateTime = new DateTime();
+        $activityData['activity_date_time'] = $activityDateTime->format("Y-m-d");
+      }
+      if (!isset($activityData['status_id']) || empty($activityData['status_id'])) {
+        $activityData['status_id'] = "Completed";
+      }
+      if (!isset($activityData['subject']) || empty($activityData['subject'])) {
+        $activityData['subject'] = "Activity added during migration of participation data from Starfish";
+      }
+      if (!isset($activityData['source_contact_id']) || empty($activityData['source_contact_id'])) {
+        $activityData['source_contact_id'] = 'user_contact_id';
+      }
+      try {
+        civicrm_api3('Activity', 'create', $activityData);
+        return TRUE;
+      }
+      catch (CiviCRM_API3_Exception $ex) {
+        $this->logger->logMesagge("Could not create invite activity with data " . json_encode($activityData)
+          . ", error from API Activity create: " . $ex->getMessage());
+      }
+    }
+    else {
+      $this->logger->logMessage("Trying to create case activity but caseId/activityTypeId is empty in data: " . json_encode($activityData), "Warning");
+    }
   }
 
   /**
@@ -91,21 +208,21 @@ class CRM_Nbrmigration_NbrParticipation {
   private function prepareParticipationData($contactId, $studyId, $sourceData) {
     $result = [
       'contact_id' => $contactId,
-      'case_type_id' => $this->_participationCaseTypeId,
+      'case_type_id' => $this->participationCaseTypeId,
       'subject' => "Selected for study " . $sourceData->study_number,
       'status_id' => "Open",
-      $this->_studyIdCustomField => $studyId,
-      $this->_studyParticipationStatusCustomField => $this->transformStatus($sourceData->status),
+      $this->studyIdCustomField => $studyId,
+      $this->studyParticipationStatusCustomField => $this->transformStatus($sourceData->status),
     ];
     if (!empty($sourceData->anon_study_participation_id)) {
-      $result[$this->_studyParticipantIdCustomField] = $sourceData->anon_study_participation_id;
+      $result[$this->studyParticipantIdCustomField] = $sourceData->anon_study_participation_id;
     }
     if (!empty($sourceData->date_invited)) {
       $dateInvited = new DateTime($sourceData->date_invited);
-      $result[$this->_dateInvitedCustomField] = $dateInvited->format("Y-m-d");
+      $result[$this->dateInvitedCustomField] = $dateInvited->format("Y-m-d");
     }
     if (!empty($sourceData->recall_group)) {
-      $result[$this->_recallGroupCustomField] = $sourceData->recall_group;
+      $result[$this->recallGroupCustomField] = $sourceData->recall_group;
     }
     return $result;
   }
@@ -120,44 +237,44 @@ class CRM_Nbrmigration_NbrParticipation {
     $sourceStatus = strtolower(trim($sourceStatus));
     switch ($sourceStatus) {
       case "accepted":
-        return $this->_acceptedStatus;
+        return $this->acceptedStatus;
         break;
       case "excluded":
-        return $this->_excludedStatus;
+        return $this->excludedStatus;
         break;
       case "invitation pending":
-        return $this->_invitationPendingStatus;
+        return $this->invitationPendingStatus;
         break;
       case "invited":
-        return $this->_invitedStatus;
+        return $this->invitedStatus;
         break;
       case "no response":
-        return $this->_noResponseStatus;
+        return $this->noResponseStatus;
         break;
       case "not participated":
-        return $this->_notParticipatedStatus;
+        return $this->notParticipatedStatus;
         break;
       case "participated":
-        return $this->_participatedStatus;
+        return $this->participatedStatus;
         break;
       case "refused":
-        return $this->_refusedStatus;
+        return $this->refusedStatus;
         break;
       case "reneged":
-        return $this->_renegedStatus;
+        return $this->renegedStatus;
         break;
       case "return to sender":
-        return $this->_returnToSenderStatus;
+        return $this->returnToSenderStatus;
         break;
       case "selected":
-        return $this->_selectedStatus;
+        return $this->selectedStatus;
         break;
       case "withdrawn":
-        return $this->_withdrawnStatus;
+        return $this->withdrawnStatus;
         break;
       default:
-        $this->_logger->logMessage("Status " . $sourceStatus . " is not valid, status Selected used.", "warning");
-        return $this->_selectedStatus;
+        $this->logger->logMessage("Status " . $sourceStatus . " is not valid, status Selected used.", "warning");
+        return $this->selectedStatus;
         break;
     }
   }
@@ -172,24 +289,24 @@ class CRM_Nbrmigration_NbrParticipation {
     $valid = TRUE;
     // sample_id should be present and not empty
     if (!isset($sourceData->sample_id) || empty($sourceData->sample_id)) {
-      $this->_logger->logMessage('Empty sample_id or no sample_id in source data with id: ' . $sourceData->id, 'error');
+      $this->logger->logMessage('Empty sample_id or no sample_id in source data with id: ' . $sourceData->id, 'error');
       $valid = FALSE;
     }
     // study_number should be present and not empty
     if (!isset($sourceData->study_number) || empty($sourceData->study_number)) {
-      $this->_logger->logMessage('Empty study_number or no study_number in source data with id: ' . $sourceData->id, 'error');
+      $this->logger->logMessage('Empty study_number or no study_number in source data with id: ' . $sourceData->id, 'error');
       $valid = FALSE;
     }
     // status should be present and not empty
     if (!isset($sourceData->status) || empty($sourceData->status)) {
-      $this->_logger->logMessage('Empty status or no status in source data with id: ' . $sourceData->id, 'error');
+      $this->logger->logMessage('Empty status or no status in source data with id: ' . $sourceData->id, 'error');
       $valid = FALSE;
     }
     else {
       // if status != selected, anon_study_participation_id should be present and not empty
       if (strtolower($sourceData->status) != "selected") {
         if (!isset($sourceData->anon_study_participation_id) || empty($sourceData->anon_study_participation_id)) {
-          $this->_logger->logMessage('Empty anon_study_participation_id or no anon_study_participation_id whilst status is not selected in source data with id: ' . $sourceData->id, 'error');
+          $this->logger->logMessage('Empty anon_study_participation_id or no anon_study_participation_id whilst status is not selected in source data with id: ' . $sourceData->id, 'error');
           $valid = FALSE;
         }
       }
